e3bc1ee6b4f15c6e77c567e948b611409c4537ef
Hannah Norish <hannahjn@outlook.com>
2018-09-12 17:06:34 +0000

    admin ability to create a new category and view a list of categories added

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,14 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
-
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
+  
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
