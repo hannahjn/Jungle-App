cf2221bf6a2b24e37f07ccf2b493e131bad231cb
Hannah Norish <hannahjn@outlook.com>
2018-09-13 22:20:50 +0000

    admin authentication added for protected pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+	http_basic_authenticate_with name: "jungle", password: "book"
+
 	def index
 		@categories = Category.order(created_at: :desc).all 
 		@category = Category.new
