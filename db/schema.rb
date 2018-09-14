1f2585de36d71f0820b3187469dfadae216748e4
Hannah Norish <hannahjn@outlook.com>
2018-09-14 15:34:39 +0000

    database table and model set up for reviews feature

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,15 @@ ActiveRecord::Schema.define(version: 20180912183339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "user_id"
+    t.text "description"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
